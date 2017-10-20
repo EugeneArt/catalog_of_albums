27c711e4dcf2bcce7a000dc10c0c8e4b24b05d93
aegean <mailbox.aea@gmail.com>
2017-10-20 23:25:30 +0300

    create tests for an album view

--- server/api/test.py
+++ server/api/test.py
@@ -1,5 +1,8 @@
 from django.test import TestCase
 from .models import Album, Track
+from rest_framework.test import APIClient
+from rest_framework import status
+from django.core.urlresolvers import reverse
 
 class ModelAlbumTestCase(TestCase):
 
@@ -28,3 +31,46 @@ class ModelTrackTestCase(TestCase):
         self.track.save()
         new_count = Track.objects.count()
         self.assertNotEqual(old_count, new_count)
+
+class ViewAlbumTestCase(TestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+        self.album = {
+            'album_name': 'Crazy Worldr',
+            'year': '1990'
+        }
+        self.response = self.client.post(
+            reverse('album_create'),
+            self.album,
+            format="json")
+
+    def test_api_can_create_a_album(self):
+        self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
+
+    def test_api_can_get_a_album(self):
+        response = self.client.get(
+            reverse('album_details', kwargs={'pk': self.album.id}),
+            format="json")
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertContains(response, self.album)
+
+    def test_api_can_update_album(self):
+        change_album = {
+            'album_name': 'Acoustica',
+            'year': '2001'
+        }
+        res = self.client.put(
+            reverse('album_details', kwargs={'pk': self.album.id}),
+            change_album, format='json'
+        )
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_api_can_delete_album(self):
+        response = self.client.delete(
+            reverse('album_details', kwargs={'pk': self.album.id}),
+            format='json',
+            follow=True)
+
+        self.assertEquals(response.status_code, status.HTTP_204_NO_CONTENT)
