000cd18630c08f0e5c170990e6f3de2839586003
aegean <mailbox.aea@gmail.com>
2017-10-24 00:27:13 +0300

    clean urls

--- server/api/urls.py
+++ server/api/urls.py
@@ -1,9 +1,7 @@
 from django.conf.urls import url
 from rest_framework.urlpatterns import format_suffix_patterns
 from .views import AlbumCreateView, AlbumDetailsView
-from rest_framework.routers import DefaultRouter
 
-router = DefaultRouter(trailing_slash=False)
 
 urlpatterns = {
     url(r'albums/?$', AlbumCreateView.as_view(), name="album_create"),
